9392ee4616cc81b6dc335b8fd2cda7fe9de769d0
DeathForRussia <ramirkiev@gmail.com>
2022-11-11 01:04:12 +0200

    Revert "MVC"
    This reverts commit ff8a38b2ce9e068ebc5778ed7795ce652e7b1ae9.

--- sprint 16/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sprint_16.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
